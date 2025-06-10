86d0dee6fdc15042c9b978b437e2213ead30f9b0
chan8632 <duscks8632@gmail.com>
2025-06-11 00:58:30 +0900

    5 2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
+  console.log("i run everyTime");
+  useEffect(() => {
+    console.log("i run once");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
