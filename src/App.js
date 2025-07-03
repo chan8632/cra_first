b09d14c3eb252833801b9f0b08e13f08e530e4a2
chan8632 <duscks8632@gmail.com>
2025-07-03 12:56:13 +0900

    6 2 coin tracker 코인의 달러 가치 가져오기 성공

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  // 코인 정보 가져오고 coin state에 정보 가져오기
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,21 +10,45 @@ function App() {
         setLoading(false);
       });
   }, []);
+  // 코인들 정보
+  const [coins, setCoins] = useState([]);
+  // 달러 state
+  const [dollar, setDollar] = useState(0);
+  // 코인 몇 개 살 수 있냐.
+  const [coinCount, setCoinCount] = useState(0);
 
+  const dollarChange = (event) => {
+    setDollar(event.target.value);
+  };
+  const coinSelect = (event) => {
+    const data = event.target.value;
+    const coinValue = parseInt(data.match(/\$(\d+)\./)[1], 10);
+    const dollarToCoinCount = Math.floor(dollar / coinValue);
+    setCoinCount(dollarToCoinCount);
+  }
   return (
     <div>
-      <h1>The Coins</h1>
+      <h1>The Coins! {loading ? null : coins.length}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <strong>Loading</strong>
       ) : (
-        <select>
+        <select onChange={coinSelect}>
           {coins.map((coin) => (
-            <option>
-              {coin.id} ({coin.symbol}) : {coin.quotes.USD.price}$
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <hr />
+      <input
+        onChange={dollarChange}
+        value={dollar}
+        type="number"
+        placeholder="달러를 작성하세요"
+      />
+      <span>$</span>
+      <div>{coinCount}개 살 수 있습니다.</div>
     </div>
   );
 }
